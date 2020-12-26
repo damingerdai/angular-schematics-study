813be3138b1f40983584aa65b3c841078490fc6d
Arthur Ming <guobin.ming@pwc.com>
2020-12-26 21:05:50 +0800

    支持文件名字格式化

--- hello-world/src/hello-world/index.ts
+++ hello-world/src/hello-world/index.ts
@@ -1,5 +1,6 @@
 import { Rule, SchematicContext, Tree, url, apply, template, mergeWith } from '@angular-devkit/schematics';
 
+import { strings } from '@angular-devkit/core';
 
 export function helloWorld(_options: any): Rule {
   return (_tree: Tree, _context: SchematicContext) => {
@@ -7,6 +8,7 @@ export function helloWorld(_options: any): Rule {
     const sourceParametrizedTemplates = apply(sourceTemplates, [
       template({
         ..._options, // 使用者所輸入的參數
+        ...strings
       })
     ]);
 
