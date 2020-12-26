446ed2274119c56ace6d3828350f66499586f5a6
Arthur Ming <guobin.ming@pwc.com>
2020-12-26 20:26:31 +0800

    test: 给hello-world例子添加测试用例

--- hello-world/src/hello-world/index_spec.ts
+++ hello-world/src/hello-world/index_spec.ts
@@ -3,14 +3,22 @@ import { SchematicTestRunner } from '@angular-devkit/schematics/testing';
 import * as path from 'path';
 
 
-const collectionPath = path.join(__dirname, '../collection.json');
+const collectionPath = path.join(__dirname, '../../src/collection.json');
 
 
 describe('hello-world', () => {
-  it('works', async () => {
+  it('default', async () => {
     const runner = new SchematicTestRunner('schematics', collectionPath);
     const tree = await runner.runSchematicAsync('hello-world', {}, Tree.empty()).toPromise();
 
-    expect(tree.files).toEqual([]);
+    expect(tree.files).toContain('/hello');
+  });
+
+  it('with name', async () => {
+    const runner = new SchematicTestRunner('schematics', collectionPath);
+    const tree = await runner.runSchematicAsync('hello-world', { name: 'damingerdai'}, Tree.empty()).toPromise();
+
+    expect(tree.files).toContain('/damingerdai');
+    expect(tree.readContent('/damingerdai')).toEqual('world');
   });
 });
