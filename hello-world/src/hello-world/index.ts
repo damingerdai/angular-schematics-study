d61e9919b6bec2b8f5a95e92f8978881310cceab
Arthur Ming <guobin.ming@pwc.com>
2020-12-26 20:53:11 +0800

    支持文件模版输出

--- hello-world/src/hello-world/index.ts
+++ hello-world/src/hello-world/index.ts
@@ -1,11 +1,15 @@
-import { Rule, SchematicContext, Tree } from '@angular-devkit/schematics';
+import { Rule, SchematicContext, Tree, url, apply, template, mergeWith } from '@angular-devkit/schematics';
 
 
-// You don't have to export the function as default. You can also have more than one rule factory
-// per file.
 export function helloWorld(_options: any): Rule {
-  return (tree: Tree, _context: SchematicContext) => {
-    tree.create(_options.name || 'hello', 'world');
-    return tree;
+  return (_tree: Tree, _context: SchematicContext) => {
+    const sourceTemplates = url('./files'); 
+    const sourceParametrizedTemplates = apply(sourceTemplates, [
+      template({
+        ..._options, // 使用者所輸入的參數
+      })
+    ]);
+
+    return mergeWith(sourceParametrizedTemplates)
   };
 }
