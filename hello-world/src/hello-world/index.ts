86c602bc6377835581b2dbffeb1bcca2813ed65a
Arthur Ming <guobin.ming@pwc.com>
2020-12-27 20:33:19 +0800

    使用Angular Schematics 的 API重构

--- hello-world/src/hello-world/index.ts
+++ hello-world/src/hello-world/index.ts
@@ -11,7 +11,11 @@ import {
   mergeWith
 } from '@angular-devkit/schematics';
 import { parseName } from '@schematics/angular/utility/parse-name';
-import { buildRelativePath } from '@schematics/angular/utility/find-module';
+import { validateName, validateHtmlSelector } from '@schematics/angular/utility/validation';
+import { buildRelativePath, findModuleFromOptions, ModuleOptions } from '@schematics/angular/utility/find-module';
+import { addDeclarationToModule } from '@schematics/angular/utility/ast-utils';
+import { InsertChange } from '@schematics/angular/utility/change';
+
 
 import * as ts from 'typescript';
 
@@ -43,6 +47,35 @@ export function helloWorld(_options: HelloSchematics): Rule {
     const defaultProjectPath = buildDefaultPath(project);
     const parsePath = parseName(defaultProjectPath, _options.name);
     const { name, path } = parsePath;
+    // 加入名字跟 selector 的校验
+    validateName(name);
+    validateHtmlSelector(`hello-${strings.dasherize(name)}`);
+    // 用 findModuleFromOptions 找出關連度最高的 module.ts 的 path
+    const moduleOptions: ModuleOptions = { name: name, path: path };
+    const modulePath = findModuleFromOptions(_tree, moduleOptions) || '';
+
+    const sourceFile = ts.createSourceFile(
+      'test.ts',
+      (_tree.read(modulePath) || []).toString(),
+      ts.ScriptTarget.Latest,
+      true
+    );
+
+    const componentPath = `${path}/hello-${strings.dasherize(name)}.component`;
+    const classifiedName = `Hello${strings.classify(name)}Component`;
+    const relativePath = buildRelativePath(modulePath, componentPath);
+    // 用 addDeclarationToModule 取得變動的內容（ InsertChange ）
+    const declarationChanges = addDeclarationToModule(sourceFile, modulePath, classifiedName, relativePath);
+
+    // apply更新
+    const declarationRecorder = _tree.beginUpdate(modulePath);
+    for (const change of declarationChanges) {
+      if (change instanceof InsertChange) {
+        declarationRecorder.insertLeft(change.pos, change.toAdd);
+      }
+    }
+    _tree.commitUpdate(declarationRecorder);
+
     const sourceTemplates = url('./files');
     const sourceParametrizedTemplates = apply(sourceTemplates, [
       template({
@@ -54,57 +87,6 @@ export function helloWorld(_options: HelloSchematics): Rule {
       move(path) // 將產生出來的檔案移到正確的目錄下
     ]);
 
-    // 获取AppModule
-    const text = _tree.read(defaultProjectPath + '/app.module.ts') || [];
-    const sourceFile = ts.createSourceFile(
-      'test.ts',
-      text.toString(), // 轉成字串後丟進去以產生檔案，方便後續操作
-      ts.ScriptTarget.Latest
-    );
-    // 获取class
-    const classDeclaration = sourceFile.statements.find(node => ts.isClassDeclaration(node))! as ts.ClassDeclaration;
-    // 获取装饰器
-    const decorator = classDeclaration.decorators![0] as ts.Decorator;
-    const callExpression = decorator.expression as ts.CallExpression;
-    const objectLiteralExpression = callExpression.arguments[0] as ts.ObjectLiteralExpression;
-    const propertyAssignment = objectLiteralExpression.properties.find((property: ts.PropertyAssignment) => {
-      return (property.name as ts.Identifier)?.escapedText === 'declarations'
-    })! as ts.PropertyAssignment;
-    const arrayLiteralExpression = propertyAssignment.initializer as ts.ArrayLiteralExpression;
-    const identifier = arrayLiteralExpression.elements[0] as ts.Identifier;
-    const declarationRecorder = _tree.beginUpdate(defaultProjectPath + '/app.module.ts');
-    // 更新 declarations
-    const changeText = identifier.getFullText(sourceFile);
-    let toInsert = '';
-    // 如果原本的字串內容有換行符號
-    if (changeText.match(/^\r?\r?\n/)) {
-      // 就把換行符號與字串前的空白加到字串裡
-      toInsert = `,${changeText.match(/^\r?\n\s*/)![0]}Hello${strings.classify(name)}Component`;
-    } else {
-      toInsert = `, Hello${strings.classify(name)}Component`;
-    }
-    declarationRecorder.insertLeft(identifier.end, toInsert);
-    //_tree.commitUpdate(declarationRecorder);
-
-    // 更新import
-    const allImports = sourceFile.statements.filter( node => ts.isImportDeclaration(node) )! as ts.ImportDeclaration[];
-    // 获取最后一个import
-    let lastImport: ts.Node | undefined;
-    for (const importNode of allImports) {
-      if ( !lastImport || importNode.getStart(sourceFile) > lastImport.getStart(sourceFile) ) {
-        lastImport = importNode;
-      }
-    }
-    const modulePath = defaultProjectPath + '/app.module.ts';
-    const componentPath = `${parsePath.path}/hello-${parsePath.name}.component.ts`;
-    const importStr = `\nimport { Hello${strings.classify(name)}Component } from '${buildRelativePath(modulePath, componentPath)}';`;
-    declarationRecorder.insertLeft(lastImport!.end, importStr);
-
-    // 更新
-    _tree.commitUpdate(declarationRecorder);
-
-    // 重新讀取檔案並印出來看看
-    console.log(_tree.read(defaultProjectPath + '/app.module.ts')!.toString());
     return mergeWith(sourceParametrizedTemplates);
   };
 }
