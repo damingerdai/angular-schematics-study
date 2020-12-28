888ea1a0286cdb972ef24e45b826f44299fb8bab
Arthur Ming <guobin.ming@pwc.com>
2020-12-28 17:17:13 +0800

    将hello-world的ut测试支持angular项目

--- hello-world/src/hello-world/index_spec.ts
+++ hello-world/src/hello-world/index_spec.ts
@@ -1,5 +1,5 @@
 import { Tree } from '@angular-devkit/schematics';
-import { SchematicTestRunner } from '@angular-devkit/schematics/testing';
+import { SchematicTestRunner, UnitTestTree } from '@angular-devkit/schematics/testing';
 import * as path from 'path';
 import { Schema as ApplicationOptions, Style } from '@schematics/angular/application/schema';
 import { Schema as WorkspaceOptions } from '@schematics/angular/workspace/schema';
@@ -10,55 +10,57 @@ const collectionPath = path.join(__dirname, '../../src/collection.json');
 
 
 describe('hello-world', () => {
-  let workspaceOptions: WorkspaceOptions;
-  let appOptions: ApplicationOptions;
+  const runner = new SchematicTestRunner('schematics', collectionPath)
+  const workspaceOptions: WorkspaceOptions = {
+    name: 'workspace',
+    newProjectRoot: 'projects',
+    version: '0.0.1'
+  };
+  const appOptions: ApplicationOptions = {
+    name: 'hello',
+    inlineStyle: false,
+    inlineTemplate: false,
+    routing: false,
+    style: Style.Scss,
+    skipTests: false,
+    skipPackageJson: false
+  }
+  const defalutOptions: HelloSchematics = {
+    name: 'feature/Arthur Ming'
+  };
+  let appTree: UnitTestTree;
 
-  beforeEach(() => {
-    workspaceOptions = {
-      name: 'workspace',
-      newProjectRoot: 'projects',
-      version: '0.0.1'
-    };
-    appOptions = {
-      name: 'hello',
-      inlineStyle: false,
-      inlineTemplate: false,
-      routing: false,
-      style: Style.Scss,
-      skipTests: false,
-      skipPackageJson: false
-    }
-  });
-
-  it('输出文件名：hello-arthur-ming.component.ts', async () => {
-    const name = "ArthurMing";
-    const runner = new SchematicTestRunner('schematics', collectionPath);
-    const tree = await runner.runSchematicAsync('hello-world', { name }, Tree.empty()).toPromise();
-
-    const dasherizeName = strings.dasherize(name);
-    const fullFileName = `/hello-${dasherizeName}.component.ts`;
-    expect(tree.files).toContain(fullFileName);
-
-    const fileContent = tree.readContent(fullFileName);
-    expect(fileContent).toMatch(/hello-arthur-ming/);
-    expect(fileContent).toMatch(/HelloArthurMingComponent/);
-  })
-
-  it('在angular项目中生成hello-arthur-ming.component.ts', async () => {
-    const options: HelloSchematics = { name: 'feature/Arthur Ming' };
-    const runner = new SchematicTestRunner('schematics', collectionPath);
-    const workpsaceTree = await runner.runExternalSchematicAsync(
+  beforeEach(async() => {
+    appTree = await runner.runExternalSchematicAsync(
       '@schematics/angular',
       'workspace',
       workspaceOptions
     ).toPromise();
-    const appTree = await runner.runExternalSchematicAsync(
+    appTree = await runner.runExternalSchematicAsync(
       '@schematics/angular',
       'application',
       appOptions,
-      workpsaceTree
+      appTree
     ).toPromise();
-    const tree = await runner.runSchematicAsync('hello-world', options, Tree.empty()).toPromise();
+
+  });
+
+  it('输出文件名：hello-arthur-ming.component.ts', async () => {
+    const options: HelloSchematics = { ...defalutOptions };
+    const tree = await runner.runSchematicAsync('hello-world', options, appTree).toPromise();
+    const fullFileName = `/projects/hello/src/app/feature/hello-arthur-ming.component.ts`;
+    expect(tree.files).toContain(fullFileName);
+  })
+
+  it('在angular项目中生成hello-arthur-ming.component.ts', async () => {
+    const options: HelloSchematics = { ...defalutOptions };
+    appTree = await runner.runExternalSchematicAsync(
+      '@schematics/angular',
+      'application',
+      { ...appOptions, name: 'world' },
+      appTree
+    ).toPromise();
+    const tree = await runner.runSchematicAsync('hello-world', options, appTree).toPromise();
     expect(tree.files).toContain('/projects/hello/src/app/feature/hello-arthur-ming.component.ts');
   })
 });
